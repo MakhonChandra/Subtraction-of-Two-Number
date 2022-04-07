5d2842b5926c801604f3dfe2945220ae98b485bb
Makhon Chandra Barman <103095870+MakhonChandra@users.noreply.github.com>
2022-04-07 11:51:28 -0700

    Add files via upload

---
+++ Subtraction.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Makhon Chandra
+
+"""
+
+num1 = int(input("Enter First Number: "))
+num2 = int(input("Enter Secound Number:"))
+result = num1 - num2
+print(num1,"-",num2,"=",result)
